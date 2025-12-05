41b4a211f31532fa004d7b86bd6ace9d02bc5561
ItsMee04 <meemeeits@gmail.com>
2025-12-05 15:00:23 +0700

    first commit

---
+++ database/migrations/2025_12_05_144902_create_cetak_nota_pembelian_procedure.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::unprepared("
+            DROP PROCEDURE IF EXISTS CetakNotaPembelian;
+        ");
+
+        DB::unprepared("
+            CREATE PROCEDURE CetakNotaPembelian(IN KODEPEMBELIAN_INPUT INT)
+            BEGIN
+                SELECT
+                    tr.kodepembelian,
+                    pl.nama AS namapelanggan,
+                    pl.alamat,
+                    pl.kontak,
+                    pg.nip,
+                    pg.nama AS namapegawai,
+                    pr.kodeproduk,
+                    pr.nama AS namaproduk,
+                    pr.berat,
+                    pr.karat,
+                    kr.harga_beli,
+                    pr.image_produk,
+                    tr.total,
+                    kr.terbilang,
+                    kr.total AS keranjangtotal
+                FROM produk pr
+                JOIN keranjang_pembelian kr ON pr.id = kr.produk_id
+                JOIN pembelian tr ON kr.kodepembelian = tr.kodepembelian
+                JOIN pelanggan pl ON tr.pelanggan_id = pl.id
+                JOIN users us ON tr.oleh = us.id
+                JOIN pegawai pg ON us.pegawai_id = pg.id
+                WHERE tr.id = KODEPEMBELIAN_INPUT;
+            END
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::unprepared("DROP PROCEDURE IF EXISTS CetakNotaPembelian;");
+    }
+};
